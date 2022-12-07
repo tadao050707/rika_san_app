a0116fc0064b87658df5285ea5d81709ea89febc
rika kaminoyama <richun.06@gmail.com>
2022-12-08 02:12:01 +0900

    利用者登録機能実装

--- app/controllers/care_users_controller.rb
+++ app/controllers/care_users_controller.rb
@@ -7,4 +7,20 @@ class CareUsersController < ApplicationController
   def new
     @care_user = CareUser.new
   end
+
+  def create
+    @care_user = CareUser.new(care_user_params)
+    if @care_user.save
+      redirect_to care_users_path, notice: "利用者登録完了！"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def care_user_params
+    params.require(:care_user).permit(:name, :birthday, :age, :sex, :availability, :division, :image).merge(sex: params[:care_user][:sex].to_i, availability: params[:care_user][:availability].to_i, division: params[:care_user][:division].to_i)
+  end
+
 end
