9ac89ed00d111349a060c22247b18152ba55b8c8
rika kaminoyama <richun.06@gmail.com>
2022-12-14 12:19:27 +0900

    deviseを使用しユーザ認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  root 'top#index'
+  resource :users, only: [:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :care_users
   resources :healths
