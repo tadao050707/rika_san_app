2ad75260e9110a197796b7ec7cfa11db6b9f6f27
rika kaminoyama <richun.06@gmail.com>
2022-12-14 12:36:42 +0900

    ゲストログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   root 'top#index'
   resource :users, only: [:show]
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :care_users
   resources :healths
